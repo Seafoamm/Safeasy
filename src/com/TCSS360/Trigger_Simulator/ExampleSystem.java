87c54b843948d5ca1549524d8a6fdfda1018e8bf
Seafoam <lu.brandon2020@gmail.com>
2021-06-12 12:21:18 -0700

    fixed example system

--- src/com/TCSS360/Trigger_Simulator/ExampleSystem.java
+++ src/com/TCSS360/Trigger_Simulator/ExampleSystem.java
@@ -3,13 +3,14 @@ package com.TCSS360.Trigger_Simulator;
 import com.TCSS360.Sensor_System.Sensor;
 import com.TCSS360.Signaling_System.Database;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class ExampleSystem {
     public static List<Sensor> mySensors;
-    public static Database myDatabase;
     public ExampleSystem()
     {
+        mySensors = new LinkedList<>();
         mySensors.add(new Sensor(1, Sensor.SensorStatus.NonDetect, Sensor.SensorTypes.Motion, false));
         mySensors.add(new Sensor(1, Sensor.SensorStatus.NonDetect, Sensor.SensorTypes.Temperature, false));
         mySensors.add(new Sensor(1, Sensor.SensorStatus.NonDetect, Sensor.SensorTypes.Water, false));
